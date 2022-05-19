0bd7c9c6866cf2f9bbe1e1faf5c394bb2d8f22d2
myeongseob1 <kwakms123@ajou.ac.kr>
2022-05-19 23:16:34 +0900

    JPA 설정 및 DB 설정 잘되는지 확인

---
+++ src/main/java/jpabook/jpashop/MemberRepository.java
@@ -0,0 +1,22 @@
+package jpabook.jpashop;
+
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Repository
+public class MemberRepository {
+    @PersistenceContext
+    private EntityManager em;
+
+    //커맨드와 쿼리를 분리하자
+    public Long save(Member member){
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member find(Long id){
+        return em.find(Member.class, id);
+    }
+}
