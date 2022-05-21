ffe924b3fe2b7be33a1d190a7307c36e77ee7ba2
myeongseob1 <kwakms123@ajou.ac.kr>
2022-05-21 16:36:50 +0900

    상품 엔티티 개발 + 공통 exception 개발

--- src/main/java/jpabook/jpashop/domain/item/Item.java
+++ src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -24,4 +25,23 @@ public abstract class Item {
 
     @ManyToMany(mappedBy = "items")
     private List<Category> categories = new ArrayList<>();
+
+    //==비즈니스 로직==//
+
+    /**
+     * stock을 quantity만큼 증가
+     * @param quantity
+     */
+    public void addStock(int quantity){
+        this.stockQuantity += stockQuantity;
+    }
+
+    public void removeStock(int quantity){
+        int resStock = this.stockQuantity - quantity;
+
+        if(resStock < 0){
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = resStock;
+    }
 }
