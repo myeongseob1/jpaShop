05d744375b21dd9461ea0fdd37c5602500e2dbd4
myeongseob1 <kwakms123@ajou.ac.kr>
2022-05-23 22:22:43 +0900

    orderService - 주문, 주문취소 기능 개발

--- src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -1,13 +1,16 @@
 package jpabook.jpashop.domain;
 
 import jpabook.jpashop.domain.item.Item;
+import lombok.AccessLevel;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
 
 @Entity
 @Getter @Setter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class OrderItem {
     @Id @GeneratedValue
     @Column(name = "order_item_id")
@@ -24,6 +27,10 @@ public class OrderItem {
     private int orderPrice; //주문 가격
     private int count; //주문 수량
 
+    //생성메서드를 이용한 생성 이외의 생성 방식을 막아버린다 -> 방식이 다 다르면 유지보수할때 어려움을 겪게될수 있음 -> 어노테이션으로 대체
+//    protected OrderItem(){
+//
+//    }
     //== 생성 메서드 ==//
     public static OrderItem createOrderItem(Item item, int orderPrice, int count){
         OrderItem orderItem = new OrderItem();
